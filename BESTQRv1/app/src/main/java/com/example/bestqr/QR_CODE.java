52933265e85568fd8e2c5698f32e3f2438cadfa8
Charles Aina <aina1@ualberta.ca>
2022-03-03 15:18:14 -0700

    Adding initial Classes

---
+++ BESTQRv1/app/src/main/java/com/example/bestqr/QR_CODE.java
@@ -0,0 +1,21 @@
+package com.example.bestqr;
+
+import android.location.Location;
+
+public class QR_CODE {
+    private Location codeLocation;
+
+
+    public QR_CODE(Location codeLocation) {
+        this.codeLocation = codeLocation;
+    }
+
+    public void generateCode(){
+
+    }
+
+    public void calculateScore(){
+
+    }
+
+}
