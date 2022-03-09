3bd4b11429c8c8bd92f72884d045a0c34b7d7ce4
SeiverB <43014287+SeiverB@users.noreply.github.com>
2022-03-08 20:31:19 -0700

    Implemented main leaderboard screen

---
+++ BESTQRv1/app/src/main/java/com/example/bestqr/ui/leaderboard/LeaderboardViewModelQR.java
@@ -0,0 +1,19 @@
+package com.example.bestqr.ui.leaderboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class LeaderboardViewModelQR extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public LeaderboardViewModelQR() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is leaderboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
--- BESTQRv1/app/src/main/java/com/example/bestqr/ui/map/MapFragment.java
+++ BESTQRv1/app/src/main/java/com/example/bestqr/ui/map/MapFragment.java
@@ -2,8 +2,11 @@ package com.example.bestqr.ui.map;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -25,6 +28,20 @@ public class MapFragment extends Fragment {
         View root = binding.getRoot();
 
         return root;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        // Report that this fragment would like to participate in populating the options menu
+        // by receiving a call to onCreateOptionsMenu(Menu, MenuInflater) and related methods.
+        setHasOptionsMenu(true);
+    }
+
+    // Can modify this function to customize top toolbar
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
