4c172c2a9287e36d25688d277917d77fbee08c35
Evan Rogers <ejrogers@ualberta.ca>
2022-03-09 23:04:58 -0700

    QR from gallery --> Hash implemented

--- BESTQRv1/app/src/main/java/com/example/bestqr/CameraActivity.java
+++ BESTQRv1/app/src/main/java/com/example/bestqr/CameraActivity.java
@@ -1,21 +1,23 @@
 package com.example.bestqr;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.location.Location;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -23,24 +25,37 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.bestqr.databinding.ActivityMainBinding;
 
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
 public class CameraActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
     // Define the pic id
-    private static final int pic_id = 123;
+    private static final int PIC_ID = 123;
+    private static final int PICK_IMAGE = 1;
 
     // Define the button and imageview type variable
     Button scanButton;
 
+    // Button to scan qr Code from gallery
+    ImageButton openGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        com.example.bestqr.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // Manually set the action bar to the toolbar specified in the layout
@@ -59,7 +74,7 @@ public class CameraActivity extends AppCompatActivity {
 
         // This should be in th login activity
         // get unique device id
-        String androidId = Settings.Secure.getString(getContentResolver(),Settings.Secure.ANDROID_ID);
+        @SuppressLint("HardwareIds") String androidId = Settings.Secure.getString(getContentResolver(),Settings.Secure.ANDROID_ID);
         // test identification of user ideally info will be taken in the signup activity and stored in firebase
         QR_CODE userIdentification = new QR_CODE(androidId);
         Profile userProfile = new Profile("UserName",userIdentification,1231231231,"emailaddress");
@@ -80,6 +95,19 @@ public class CameraActivity extends AppCompatActivity {
                 intentIntegrator.initiateScan();
             }
         });
+
+        // Choose image from gallery
+        openGallery = (ImageButton) this.findViewById(R.id.openGallery);
+        openGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent photoPickerIntent = new Intent();
+                photoPickerIntent.setType("image/*");
+                photoPickerIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(photoPickerIntent, "Select Image"), PICK_IMAGE);
+            }
+        });
     }
 
     @Override
@@ -89,33 +117,88 @@ public class CameraActivity extends AppCompatActivity {
         IntentResult intentResult = IntentIntegrator.parseActivityResult(
                 requestCode,resultCode,data
         );
-        //Check condition
-        if (intentResult.getContents()!= null){
-            //When result content is not null
-            //Initialize alert dialog
-            AlertDialog.Builder builder = new AlertDialog.Builder(
-                    CameraActivity.this
-            );
-            //Set title
-            builder.setTitle("Score = ");
-            //Set score(but currently is set message)
-            builder.setMessage(intentResult.getContents());
-            //Set positive button
-            builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    //Dismiss dialog
-                    dialogInterface.dismiss();
-                }
-            });
-            //Show alert dialog
-            builder.show();
-        }else{
-            //When result content is null
-            //Display toast
-            Toast.makeText(getApplicationContext()
-                    ,"sorry, nothing is scanned", Toast.LENGTH_SHORT)
-                    .show();
+
+        MessageDigest digest;
+        String qrHash;
+
+        // If user takes photo with camera
+        if (requestCode == PIC_ID) {
+            //Check condition
+            if (intentResult.getContents() != null) {
+                //When result content is not null
+                //Initialize alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        CameraActivity.this
+                );
+                //Set title
+                builder.setTitle("Score = ");
+                //Set score(but currently is set message)
+                builder.setMessage(intentResult.getContents());
+                //Set positive button
+                builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Dismiss dialog
+                        dialogInterface.dismiss();
+                    }
+                });
+                //Show alert dialog
+                builder.show();
+            } else {
+                //When result content is null
+                //Display toast
+                Toast.makeText(getApplicationContext()
+                        , "sorry, nothing is scanned", Toast.LENGTH_SHORT)
+                        .show();
+            }
         }
+
+        // If user chooses an image from the gallery
+        if (requestCode == PICK_IMAGE) {
+
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                try {
+                    Bitmap bMap = selectedImage;
+                    String contents = null;
+                    int[] intArray = new int[bMap.getWidth()*bMap.getHeight()];
+                    bMap.getPixels(intArray, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+                    LuminanceSource source = new RGBLuminanceSource(bMap.getWidth(), bMap.getHeight(), intArray);
+                    BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+                    MultiFormatReader reader = new MultiFormatReader();
+                    Result result = reader.decode(bitmap);
+                    contents = result.getText();
+                    digest = MessageDigest.getInstance("SHA-256");
+                    byte[] encodedHash = digest.digest(
+                            contents.getBytes(StandardCharsets.UTF_8));
+                    qrHash = bytesToHex(encodedHash);
+                    Toast.makeText(getApplicationContext(),qrHash,Toast.LENGTH_LONG).show();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+
+            } catch (FileNotFoundException e) {
+
+                e.printStackTrace();
+
+                Toast.makeText(CameraActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
     }
 }
--- BESTQRv1/app/src/main/java/com/example/bestqr/ui/map/MapFragment.java
+++ BESTQRv1/app/src/main/java/com/example/bestqr/ui/map/MapFragment.java
@@ -2,11 +2,8 @@ package com.example.bestqr.ui.map;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,34 +11,23 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.bestqr.databinding.FragmentMapBinding;
 
+import com.example.bestqr.ui.camera.CameraViewModel;
+
+
 public class MapFragment extends Fragment {
 
-    private MapViewModel mapViewModel;
+    private CameraViewModel mapViewModel;
     private FragmentMapBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         mapViewModel =
-                new ViewModelProvider(this).get(MapViewModel.class);
+                new ViewModelProvider(this).get(CameraViewModel.class);
 
         binding = FragmentMapBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         return root;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        // Report that this fragment would like to participate in populating the options menu
-        // by receiving a call to onCreateOptionsMenu(Menu, MenuInflater) and related methods.
-        setHasOptionsMenu(true);
-    }
-
-    // Can modify this function to customize top toolbar
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
