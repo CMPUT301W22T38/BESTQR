2084442e0d6750d35bff6ef55384cf0ba7c96e12
yusuke_yamashiro <yusuke.y.nz1213@gmail.com>
2022-03-10 17:34:31 -0700

    added CameraActivityTest file for UI testing

---
+++ BESTQRv1/app/src/androidTest/java/com/example/bestqr/CameraActivityTest.java
@@ -0,0 +1,40 @@
+package com.example.bestqr;
+
+import android.app.Activity;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class CameraActivityTest {
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<CameraActivity> rule=
+            new ActivityTestRule<>(CameraActivity.class, true, true);
+
+    @Before
+    public void setUp()throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+
+    }
+
+    @Test
+    public void start()throws Exception{
+        Activity activity = rule.getActivity();
+    }
+    
+
+    @After
+    public void tearDown()throws Exception{
+        solo.finishOpenedActivities();
+    }
+
+
+}
