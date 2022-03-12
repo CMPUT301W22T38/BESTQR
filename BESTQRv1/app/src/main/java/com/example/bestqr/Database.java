90f59755019f213b4f923d7bec20df8aa0623601
Jay Park <youngcha@ualberta.ca>
2022-03-12 00:01:55 -0700

    stable

--- BESTQRv1/app/src/main/java/com/example/bestqr/Database.java
+++ BESTQRv1/app/src/main/java/com/example/bestqr/Database.java
@@ -1,9 +1,11 @@
 package com.example.bestqr;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Database {
+public class Database extends AppCompatActivity {
     private FirebaseDatabase db;
     private DatabaseReference root_ref;
     private DatabaseReference users_ref;
