d0ee19d0f7e7a46d6c04b74cc2a686c2e5929cc3
yusuke_yamashiro <yusuke.y.nz1213@gmail.com>
2022-03-14 11:44:13 -0600

    Implemented UI test for basic camera activity

--- BESTQRv1/app/src/androidTest/java/com/example/bestqr/CameraActivityTest.java
+++ BESTQRv1/app/src/androidTest/java/com/example/bestqr/CameraActivityTest.java
@@ -1,4 +1,68 @@
 package com.example.bestqr;
 
+import android.app.Activity;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
 public class CameraActivityTest {
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<CameraActivity> rule=
+            new ActivityTestRule<>(CameraActivity.class, true, true);
+
+    @Before
+    public void setUp()throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+
+    }
+
+    @Test
+    public void start()throws Exception{
+        Activity activity = rule.getActivity();
+    }
+
+    @Test
+    public void CameraTest(){
+        solo.assertCurrentActivity("Wrong activity", CameraActivity.class);
+        solo.clickOnButton("Scan");
+        solo.clickOnImageButton(0);
+
+    }
+
+//    @Test
+//    public void LeaderBordTest(){
+//        solo.assertCurrentActivity("Wrong activity", CameraActivity.class);
+//        solo.waitForFragmentById(R.id.navigation_leaderboard_main, 2000);
+//
+//    }
+//
+//    @Test
+//    public void UserTest(){
+//        solo.assertCurrentActivity("Wrong activity", CameraActivity.class);
+//        solo.waitForFragmentByTag("User", 2000);
+//    }
+//
+//    @Test
+//    public void MapTest(){
+//        solo.assertCurrentActivity("Wrong activity", CameraActivity.class);
+//        solo.waitForFragmentByTag("Map", 2000);
+//    }
+
+
+
+    @After
+    public void tearDown()throws Exception{
+        solo.finishOpenedActivities();
+    }
+
+
 }
