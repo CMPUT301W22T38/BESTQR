f7d92d067bc4976a3c7a25aa54289e315e052071
yusuke_yamashiro <yusuke.y.nz1213@gmail.com>
2022-03-14 15:27:26 -0600

    added scoring unit test, deleted some tests that have error

--- BESTQRv1/app/src/test/java/com/example/bestqr/ScoringTest.java
+++ BESTQRv1/app/src/test/java/com/example/bestqr/ScoringTest.java
@@ -2,5 +2,27 @@ package com.example.bestqr;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * This class tests the calculation of the score.(content->hash, hash->score)
+ */
 public class ScoringTest {
+    /**
+     * test content->hash
+     */
+    @Test
+    public void calculateHashTest() {
+        String hex = QRCODE.calculateHash("BFG5DGW54");
+        assertEquals("696ce4dbd7bb57cbfe58b64f530f428b74999cb37e2ee60980490cd9552de3a6", hex);
+        //add more test here
+    }
+
+    /**
+     * Test hash -> score
+     */
+    @Test
+    public void calculateScoreTest(){
+        int score = QRCODE.calculateScore("696ce4dbd7bb57cbfe58b64f530f428b74999cb37e2ee60980490cd9552de3a6");
+        assertEquals(111, score);
+        //add more test for scoring here
+    }
 }
