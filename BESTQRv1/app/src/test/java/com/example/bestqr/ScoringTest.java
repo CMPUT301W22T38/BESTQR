2d435d6e10c4521f9624b4380ca1fdd8c397dee8
yusuke_yamashiro <yusuke.y.nz1213@gmail.com>
2022-03-16 09:25:23 -0600

    deleted testImplementation 'org.junit.jupiter:junit-jupiter'

--- BESTQRv1/app/src/test/java/com/example/bestqr/ScoringTest.java
+++ BESTQRv1/app/src/test/java/com/example/bestqr/ScoringTest.java
@@ -11,8 +11,11 @@ public class ScoringTest {
      */
     @Test
     public void calculateHashTest() {
-        String hex = QRCODE.calculateHash("BFG5DGW54");
-        assertEquals("696ce4dbd7bb57cbfe58b64f530f428b74999cb37e2ee60980490cd9552de3a6", hex);
+        String hash= QRCODE.calculateHash("BFG5DGW54");
+        assertEquals("696ce4dbd7bb57cbfe58b64f530f428b74999cb37e2ee60980490cd9552de3a6", hash);
+        hash = QRCODE.calculateHash("bfg5dgw54");
+        assertEquals("373b65a3205259aa4d0417298fc1a09051ab2543b8ddba8a277957b7c63e64fb", hash); //same contents but lower cases
+
         //add more test here
     }
 
@@ -21,8 +24,12 @@ public class ScoringTest {
      */
     @Test
     public void calculateScoreTest(){
-        int score = QRCODE.calculateScore("696ce4dbd7bb57cbfe58b64f530f428b74999cb37e2ee60980490cd9552de3a6");
+        long score = QRCODE.calculateScore("696ce4dbd7bb57cbfe58b64f530f428b74999cb37e2ee60980490cd9552de3a6");
         assertEquals(111, score);
+        score = QRCODE.calculateScore("00000000000000000000000000000000000"); // test the big score
+        assertEquals(2.0e35, score);
+        score = QRCODE.calculateScore("0123456789abcdefghijklmnopqrstuvwxyz"); // test no repeated digit
+        assertEquals(35, score);
         //add more test for scoring here
     }
 }
