c52b819aa2aa11a1782079374ef9a3835a1dc706
yusuke_yamashiro <yusuke.y.nz1213@gmail.com>
2022-03-16 10:18:58 -0600

    added extra tests to calculateHashTesti. Test failed, need discussion

--- BESTQRv1/app/src/test/java/com/example/bestqr/ScoringTest.java
+++ BESTQRv1/app/src/test/java/com/example/bestqr/ScoringTest.java
@@ -13,10 +13,15 @@ public class ScoringTest {
     public void calculateHashTest() {
         String hash= QRCODE.calculateHash("BFG5DGW54");
         assertEquals("696ce4dbd7bb57cbfe58b64f530f428b74999cb37e2ee60980490cd9552de3a6", hash);
-        hash = QRCODE.calculateHash("bfg5dgw54");
-        assertEquals("373b65a3205259aa4d0417298fc1a09051ab2543b8ddba8a277957b7c63e64fb", hash); //same contents but lower cases
+        hash = QRCODE.calculateHash("bfg5dgw54");//same contents but lower cases
+        assertEquals("373b65a3205259aa4d0417298fc1a09051ab2543b8ddba8a277957b7c63e64fb", hash);
+        hash = QRCODE.calculateHash("ABCDEFGH"); //Alphabet only
+        assertEquals("ca8c128e9d9e4a28ef24d0508aa20b5cf880604eacd8f65c0e366f7e0cc5fbcf", hash);
+        hash = QRCODE.calculateHash("123456789"); //Number only
+        assertEquals("6d78392a5886177fe5b86e585a0b695a2bcd01a05504b3c4e38bc8eeb21e8326", hash);
 
         //add more test here
+
     }
 
     /**
