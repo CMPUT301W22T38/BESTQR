d1e4c3a851e80a29f5f44c1d45ae471ce6fdfc49
Charles Aina <aina1@ualberta.ca>
2022-03-17 13:10:48 -0600

    ignore

--- BESTQRv1/app/src/main/java/com/example/bestqr/ui/map/MapFragment.java
+++ BESTQRv1/app/src/main/java/com/example/bestqr/ui/map/MapFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
