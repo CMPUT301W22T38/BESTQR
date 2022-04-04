c60dadb51e9980d7503b90204610921cac5cbd08
Evan Rogers <ejrogers@ualberta.ca>
2022-04-04 12:13:30 -0600

    Added searching for QR codes near specific LatLng

--- BESTQRv1/app/src/main/java/com/example/bestqr/ui/map/MapsFragment.java
+++ BESTQRv1/app/src/main/java/com/example/bestqr/ui/map/MapsFragment.java
@@ -21,6 +21,9 @@ import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.bestqr.Database.Database;
@@ -58,6 +61,9 @@ public class MapsFragment extends Fragment {
     SupportMapFragment supportMapFragment;
     LatLng userLatLng;
     QRCodeList allQRCodes = Database.getAllCodes();
+    Button geoSearchButton;
+    EditText searchLat;
+    EditText searchLng;
 
     @Nullable
     @Override
@@ -68,9 +74,15 @@ public class MapsFragment extends Fragment {
         //Initialize view
         View view = inflater.inflate(R.layout.fragment_maps, container, false);
 
+        searchLat = (EditText) view.findViewById(R.id.editLatitude);
+        searchLng = (EditText) view.findViewById(R.id.editLongitude);
+        geoSearchButton = (Button) view.findViewById(R.id.geoSearchButton);
+
         //Initialize map fragment
         supportMapFragment = (SupportMapFragment)
                 getChildFragmentManager().findFragmentById(R.id.map);
+
+
 
         // Initialize fused location
         client = LocationServices.getFusedLocationProviderClient(mapContext);
@@ -89,6 +101,35 @@ public class MapsFragment extends Fragment {
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
         }
 
+        geoSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    double lat = Double.parseDouble(searchLat.getText().toString());
+                    double lng = Double.parseDouble(searchLng.getText().toString());
+
+                    LatLng searchLatLng = new LatLng(lat, lng);
+
+                    // Create marker options
+                    MarkerOptions options = new MarkerOptions().position(searchLatLng)
+                            .title("Searched location");
+
+                    qrMap.animateCamera(CameraUpdateFactory.newLatLngZoom(searchLatLng, 10));
+
+                    // Add marker on map
+                    qrMap.addMarker(options);
+
+                    plotNearbyCodes(searchLatLng, 5000);
+
+
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
         return view;
     }
 
@@ -104,6 +145,8 @@ public class MapsFragment extends Fragment {
                     supportMapFragment.getMapAsync(new OnMapReadyCallback() {
                         @Override
                         public void onMapReady(@NonNull GoogleMap googleMap) {
+
+                            qrMap = googleMap;
 
                             // Initialize lat lng
                             userLatLng = new LatLng(location.getLatitude(), location.getLongitude());
@@ -122,7 +165,7 @@ public class MapsFragment extends Fragment {
                                 @Override
                                 public void onMapLoaded() {
 
-                                    plotNearbyCodes(googleMap, 5000);
+                                    plotNearbyCodes(userLatLng, 5000);
 
                                 }
                             });
@@ -152,7 +195,7 @@ public class MapsFragment extends Fragment {
         mapContext = context;
     }
 
-    public void plotNearbyCodes(GoogleMap googleMap, double maxDistance) {
+    public void plotNearbyCodes(LatLng center, double maxDistance) {
 
         for (QRCODE qrcode: allQRCodes) {
 
@@ -160,9 +203,9 @@ public class MapsFragment extends Fragment {
 
                 LatLng qrLatLng = new LatLng(qrcode.getCodeLocation().getLatitude(), qrcode.getCodeLocation().getLongitude());
 
-                if (SphericalUtil.computeDistanceBetween(userLatLng, qrLatLng) < maxDistance) {
+                if (SphericalUtil.computeDistanceBetween(center, qrLatLng) < maxDistance) {
 
-                    googleMap.addMarker(new MarkerOptions()
+                    qrMap.addMarker(new MarkerOptions()
                             .position(new LatLng(qrcode.getCodeLocation().getLatitude(), qrcode.getCodeLocation().getLongitude()))
                             .title("Score: " + qrcode.getScore()));
                 }
